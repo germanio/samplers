c596d43b2ae240e189b15c24bf0e7d5d92be09fc
Laura Lus <mlaura.lus@gmail.com>
2017-09-11 00:17:46 -0300

    recorder service finished, recording fragment unfinished

--- samplersFramework/src/main/java/org/cientopolis/samplers/service/RecordingService.java
+++ samplersFramework/src/main/java/org/cientopolis/samplers/service/RecordingService.java
@@ -3,8 +3,15 @@ package org.cientopolis.samplers.service;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.TimerTask;
 
 /**
  * Created by laura on 06/09/17.
@@ -12,6 +19,17 @@ import android.support.annotation.Nullable;
 
 public class RecordingService extends Service {
     private MediaRecorder mRecorder;
+    private long mStartingTimeMillis = 0;
+    private long mElapsedMillis = 0;
+    private int mElapsedSeconds = 0;
+    private TimerTask mIncrementTimerTask = null;
+
+    /*MultimediaIOManagement future resposibility*/
+    private String mFileName = null;
+    private String mFilePath = null;
+
+    /**/
+    private static final String LOG_TAG = "RecordingService";
 
     @Nullable
     @Override
@@ -23,11 +41,83 @@ public class RecordingService extends Service {
         void onTimerChanged(int seconds);
     }
 
+    public void setFileNameAndPath(){
+        int count = 0;
+        File f;
+
+        do{
+            count++;
+
+            mFileName = String.format("%d."+"mp4", System.currentTimeMillis());
+            mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFilePath += "/SoundRecorder/" + mFileName;
+
+            f = new File(mFilePath);
+        }while (f.exists() && !f.isDirectory());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startRecording();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mRecorder != null) {
+            stopRecording();
+        }
+
+        super.onDestroy();
+    }
 
     public void startRecording(){
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mRecorder.setOutputFile(mFilePath);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mRecorder.setAudioChannels(1);
+        if (MySharedPreferences.getPrefHighQuality(this)) {
+            mRecorder.setAudioSamplingRate(44100);
+            mRecorder.setAudioEncodingBitRate(192000);
+        }
 
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+            mStartingTimeMillis = System.currentTimeMillis();
+
+            //startTimer();
+            //startForeground(1, createNotification());
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
     }
+
+    public void stopRecording() {
+        mRecorder.stop();
+        mElapsedMillis = (System.currentTimeMillis() - mStartingTimeMillis);
+        mRecorder.release();
+        Toast.makeText(this, "recording saved to" + mFilePath, Toast.LENGTH_LONG).show();
+
+        //remove notification
+        if (mIncrementTimerTask != null) {
+            mIncrementTimerTask.cancel();
+            mIncrementTimerTask = null;
+        }
+
+        mRecorder = null;
+        /*MultimediaIOManagement responsibility
+        try {
+            mDatabase.addRecording(mFileName, mFilePath, mElapsedMillis);
+
+        } catch (Exception e){
+            Log.e(LOG_TAG, "exception", e);
+        }
+        */
+    }
+
+
 }
--- samplersFramework/src/main/java/org/cientopolis/samplers/ui/take_sample/SoundRecordFragment.java
+++ samplersFramework/src/main/java/org/cientopolis/samplers/ui/take_sample/SoundRecordFragment.java
@@ -1,14 +1,22 @@
 package org.cientopolis.samplers.ui.take_sample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.cientopolis.samplers.R;
 import org.cientopolis.samplers.model.Step;
 import org.cientopolis.samplers.model.StepResult;
+import org.cientopolis.samplers.service.RecordingService;
+
+import java.io.File;
 
 /**
  * Created by laura on 06/09/17.
@@ -34,8 +42,6 @@ public class SoundRecordFragment extends StepFragment {
         mRecordButton = (Button) rootView.findViewById(R.id.btnStart);
         mPauseButton = (Button)  rootView.findViewById(R.id.btnPause);
         mPauseButton.setVisibility(View.GONE); //pause does not show until start recording
-
-
     }
 
     @Override
@@ -51,5 +57,63 @@ public class SoundRecordFragment extends StepFragment {
     @Override
     protected StepResult getStepResult() {
         return null;
+    }
+
+    /*functionality*/
+    private void onRecord(boolean start){
+
+        Intent intent = new Intent(getActivity(), RecordingService.class);
+
+        if (start) {
+            // start recording
+            mRecordButton.setImageResource(R.drawable.ic_media_stop);
+            //mPauseButton.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(),R.string.toast_recording_start,Toast.LENGTH_SHORT).show();
+            File folder = new File(Environment.getExternalStorageDirectory() + "/SoundRecorder");
+            if (!folder.exists()) {
+                //folder /SoundRecorder doesn't exist, create the folder
+                folder.mkdir();
+            }
+
+            //start Chronometer
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            mChronometer.start();
+            mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+                @Override
+                public void onChronometerTick(Chronometer chronometer) {
+                    if (mRecordPromptCount == 0) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
+                    } else if (mRecordPromptCount == 1) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + "..");
+                    } else if (mRecordPromptCount == 2) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + "...");
+                        mRecordPromptCount = -1;
+                    }
+
+                    mRecordPromptCount++;
+                }
+            });
+
+            //start RecordingService
+            getActivity().startService(intent);
+            //keep screen on while recording
+            getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
+            mRecordPromptCount++;
+
+        } else {
+            //stop recording
+            mRecordButton.setImageResource(R.drawable.ic_mic_white_36dp);
+            //mPauseButton.setVisibility(View.GONE);
+            mChronometer.stop();
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            timeWhenPaused = 0;
+            mRecordingPrompt.setText(getString(R.string.record_prompt));
+
+            getActivity().stopService(intent);
+            //allow the screen to turn off again once recording is finished
+            getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 }
